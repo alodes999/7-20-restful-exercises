399ce300beb939c3628bc04d935c24661b5eb72c
Andrew Lodes <alodes999@gmail.com>
2015-07-20 12:34:45 -0500

    Set up blank file structure
    Moved user model to models folder

---
+++ app.rb
@@ -0,0 +1,34 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'pry'
+require 'active_record'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/cross_origin'
+require 'bcrypt'
+
+configure :development do
+  require 'sqlite3'
+  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'your_database.db')
+end
+
+configure :production do  
+  require 'pg'
+  db = URI.parse(ENV['DATABASE_URL'])
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
+#models
+
+require_relative "models/user.rb"
+#controllers
+
