6f30d3e95bf3bf354dd68bb18a674c39c3e86da0
Andrew Lodes <alodes999@gmail.com>
2015-07-20 18:37:24 -0500

    Move table creation out of class decs

--- models/story.rb
+++ models/story.rb
@@ -1,11 +1,13 @@
+unless ActiveRecord::Base.connection.table_exists?(:stories)
+  ActiveRecord::Base.connection.create_table :stories do |s|
+    s.string :title
+    s.integer :user_id
+  end
+end
+
 class Story < ActiveRecord::Base
   belongs_to :users
   
-  unless ActiveRecord::Base.connection.table_exists?(:stories)
-    ActiveRecord::Base.connection.create_table :stories do |s|
-      s.string :title
-      s.integer :user_id
-    end
-  end
   
 end
+
--- models/user.rb
+++ models/user.rb
@@ -1,14 +1,16 @@
+unless ActiveRecord::Base.connection.table_exists?(:users)
+  ActiveRecord::Base.connection.create_table :users do |c|
+    c.string :email
+    c.text :password
+  end
+end
+
+
 class User < ActiveRecord::Base
   has_many :stories
   
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   
-  unless ActiveRecord::Base.connection.table_exists?(:users)
-    ActiveRecord::Base.connection.create_table :users do |c|
-      c.string :email
-      c.text :password
-    end
-  end
-  
 end
+
