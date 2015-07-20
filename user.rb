f6c74e552afaa5ccf6cf980a0f267484f669554d
Andrew Lodes <alodes999@gmail.com>
2015-07-20 12:30:26 -0500

    New app.rb/new user model
    user model defined with table attributes

---
+++ user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  
+  unless ActiveRecord::Base.connection.table_exists?(:users)
+    ActiveRecord::Base.connection.create_table :users do |c|
+      c.string :email
+      c.text :password
+    end
+  end
+  
+end
