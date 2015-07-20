fb60c844eecbd9a016d54e638c8a40bdd4b6bcde
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-20 16:52:45 -0500

    removed binding.pry

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -17,7 +17,6 @@ end
 post "/verify_login" do
   attempted_password = params["user"]["password"]
   @user = User.where("email" => params["user"]["email"])
-  binding.pry
   # Assuming there is a user with the given email address...
   # Make a new BCrypt object with the **password from the database**.
   actual_password = BCrypt::Password.new(@user[0].password)
