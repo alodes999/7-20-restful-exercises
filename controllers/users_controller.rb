cb4bdbb7a4d2201bcd7122861f458d6c951f925e
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-20 13:44:42 -0500

    Added delete CRUD

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -10,4 +10,17 @@ end
 
 post "/users" do
   @new_user = Users.create({"email" => params["user"]["email"], "password" => params["user"]["password"]})
+  
+  redirect "/users"
+end
+
+get "/users/delete" do
+  @users = Users.all
+  erb :"/users/delete"
+end
+
+delete "/users/:x" do
+  users.delete(params["user"]["id"])
+  
+  redirect "/users"
 end
